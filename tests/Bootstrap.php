eeab615499386c8e8f1fc523997a18c3a877359b
Leonardo Grasso <me@leonardograsso.com>
2015-04-18 15:19:49 +0200

    TestConfiguration fixed

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -21,24 +21,11 @@ if ('@package_version@' !== $phpUnitVersion && version_compare($phpUnitVersion, 
 unset($phpUnitVersion);
 
 /*
- * Determine the root, library, and tests directories
- */
-$root = realpath(dirname(__DIR__));
-$coreLibrary = "$root/library";
-$coreTests = "$root/tests";
-
-
-/*
  * Load the user-defined test configuration file, if it exists; otherwise, load
  * the default configuration.
  */
-if (is_readable($coreTests . DIRECTORY_SEPARATOR . 'TestConfiguration.php')) {
-    require_once $coreTests . DIRECTORY_SEPARATOR . 'TestConfiguration.php';
+if (is_readable(__DIR__ . DIRECTORY_SEPARATOR . 'TestConfiguration.php')) {
+    require_once __DIR__ . DIRECTORY_SEPARATOR . 'TestConfiguration.php';
 } else {
-    require_once $coreTests . DIRECTORY_SEPARATOR . 'TestConfiguration.php.dist';
+    require_once __DIR__ . DIRECTORY_SEPARATOR . 'TestConfiguration.php.dist';
 }
-
-/*
- * Unset global variables that are no longer needed.
- */
-unset($root, $coreLibrary, $coreTests);
